c0cf43cca5cef088e8ac701f5b908e5f28407d7b
Mark Joshua <joshuakillurama1717@gmail.com>
2025-12-09 21:05:07 +0800

    fixed cross origin

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:8000", "http://localhost:8000"],
+  origin: ["http://localhost:8000", "http://localhost:5173"],
+  // origin: "*",
   credentials: true,
   optionsSuccessStatus: 200,
 };
